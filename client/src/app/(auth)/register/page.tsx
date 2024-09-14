b14ae75e2f0fba82d0c4da5b7ad42aab998b5989
Stroller15 <shubhamverma6351@gmail.com>
2024-09-14 15:33:53 +0530

    build rigister page

--- client/src/app/(auth)/register/page.tsx
+++ client/src/app/(auth)/register/page.tsx
@@ -4,16 +4,25 @@ import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import React from "react";
 
-const LoginPage = () => {
+const RegisterPage = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="w-[550px] bg-white p-5 rounded-xl shadow-md">
         <h1 className="text-2xl md:text-3xl lg:text-4xl font-extrabold text-center">
           Flickit
         </h1>
-        <h1 className="mt-2 text-2xl font-bold">Login</h1>
+        <h1 className="mt-2 text-2xl font-bold">Register</h1>
         <p>Welcome back to flickit!</p>
         <form action="">
+        <div className="mt-4">
+            <Label htmlFor="name">Name</Label>
+            <Input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Enter your name..."
+            />
+          </div>
           <div className="mt-4">
             <Label htmlFor="email">Email</Label>
             <Input
@@ -32,21 +41,25 @@ const LoginPage = () => {
               placeholder="Enter your password..."
             />
           </div>
-          <div className="text-right text-bold mt-1">
-            <Link href="/forget-password" className="underline">
-              Forget Password?
-            </Link>
+          <div className="mt-4">
+            <Label htmlFor="cpassword">Password</Label>
+            <Input
+              type="password"
+              id="cpassword"
+              name="confirm_password"
+              placeholder="Confirm your password..."
+            />
           </div>
           <div className="mt-4">
             <Button className="w-full">Submit</Button>
           </div>
         </form>
         <div className="mt-2">
-            <h1>Don&apos;t have an account? <strong><Link className="font-bold underline" href="/register">Register</Link></strong></h1>
+            <h1>Already have an account? <strong><Link className="font-bold underline" href="/login">Login</Link></strong></h1>
         </div>
       </div>
     </div>
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
