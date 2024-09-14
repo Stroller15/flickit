16ae61fa4d329fc6a44dc67306665199f1f4a05f
Stroller15 <shubhamverma6351@gmail.com>
2024-09-14 17:59:23 +0530

    setup ejs template for server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,19 +1,35 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response, urlencoded } from "express";
 import dotenv from "dotenv";
+import path from "path";
+import {fileURLToPath} from "url"
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+
+
+const app: Application = express();
 
 
 // Default middleware
 dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
+// View engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
 
 
 // Custom middleware
-const app: Application = express();
+
 
 // Routes
-
 app.get("/ping", (req: Request, res: Response) => {
   res.send("Pong Pong... 🚀🚀🚀🚀")
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.render("welcome")
 });
 
 export default app;
